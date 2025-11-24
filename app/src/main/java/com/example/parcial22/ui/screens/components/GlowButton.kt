9560741c5794393e739c980265aeadbc67f10dba
johan042006 <johanhernandop37@gmail.com>
2025-11-24 08:08:32 -0500

    Agregacion de los componentes, menu y button

--- app/src/main/java/com/example/parcial22/ui/screens/components/GlowButton.kt
+++ app/src/main/java/com/example/parcial22/ui/screens/components/GlowButton.kt
@@ -1,2 +1,37 @@
 package com.example.parcial22.ui.screens.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.parcial22.ui.theme.PurpleNeon
+import com.example.parcial22.ui.theme.GlowColor
+
+@Composable
+fun GlowButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .shadow(20.dp, ambientColor = GlowColor, spotColor = GlowColor)
+            .background(PurpleNeon)
+            .clickable { onClick() }
+            .padding(vertical = 14.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
+}
--- app/src/main/java/com/example/parcial22/ui/screens/components/NeonText.kt
+++ app/src/main/java/com/example/parcial22/ui/screens/components/NeonText.kt
@@ -1,2 +1,39 @@
 package com.example.parcial22.ui.screens.components
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.parcial22.ui.theme.PurpleNeon
+
+@Composable
+fun NeonText(
+    text: String,
+    modifier: Modifier = Modifier,
+    fontSize: androidx.compose.ui.unit.TextUnit = 28.sp,
+    glow: Boolean = false
+) {
+
+    val glowModifier = if (glow) {
+        modifier.shadow(
+            elevation = 20.dp,
+            ambientColor = PurpleNeon,
+            spotColor = PurpleNeon
+        )
+    } else modifier
+
+    Text(
+        text = text,
+        modifier = glowModifier.padding(4.dp),
+        color = PurpleNeon,
+        fontSize = fontSize,
+        fontWeight = FontWeight.Bold
+    )
+}
+
